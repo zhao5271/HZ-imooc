131b7f4682540a1f8e535f29b2ba28ba20fca6b5
simpleSky12 <905781330@qq.com>
2019-11-30 12:41:16 +0800

    定期更新

---
+++ README.md
@@ -0,0 +1,1 @@
+"# HZ-imooc"  git init git add README.md git commit -m "first commit" git remote add origin https://github.com/simpleSky12/HZ-imooc.git git push -u origin master
---
+++ miniprogram_npm/lin-ui/utils/util.js
@@ -0,0 +1,47 @@
+const promisic = function (func) {
+    return function (params = {}) {
+        return new Promise((resolve, reject) => {
+            const args = Object.assign(params, {
+                success: (res) => {
+                    resolve(res);
+                },
+                fail: (error) => {
+                    reject(error);
+                }
+            });
+            func(args);
+        });
+    };
+};
+
+
+const combination = function (arr, size) {
+    var r = [];
+
+    function _(t, a, n) {
+        if (n === 0) {
+            r[r.length] = t;
+            return;
+        }
+        for (var i = 0, l = a.length - n; i <= l; i++) {
+            var b = t.slice();
+            b.push(a[i]);
+            _(b, a.slice(i + 1), n - 1);
+        }
+    }
+
+    _([], arr, size);
+    return r;
+}
+
+const px2rpx = function (pxNumber) {
+    const {screenWidth} = wx.getSystemInfoSync();
+    const rpxNumber = (750 / screenWidth) * pxNumber;
+    return rpxNumber;
+};
+
+export {
+    promisic,
+    px2rpx,
+    combination
+};
